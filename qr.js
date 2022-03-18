984f0d372bd6f6812ff4f31e9f8ebf39ea497f11
Shen02bro <101873402+Shen02bro@users.noreply.github.com>
2022-03-18 17:36:20 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -8,18 +8,18 @@ WhatsAsena - Yusuf Usta
 
 const chalk = require('chalk');
 const {WAConnection} = require('@adiwajshing/baileys');
-const {StringSession} = require('./whatsasena/');
+const {StringSession} = require('./redblacky/');
 const fs = require('fs');
 
-async function whatsAsena () {
+async function RedBlacky () {
     const conn = new WAConnection();
     const Session = new StringSession();  
     conn.logger.level = 'warn';
     conn.regenerateQRIntervalMs = 30000;
     
     conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
-${chalk.white.italic('AsenaString Kodu Alıcı')}
+        console.log(`${chalk.green.bold('Red')}${chalk.blue.bold('Blacky')}
+${chalk.white.italic('BlackyString Kodu Alıcı')}
 
 ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
     });
@@ -28,11 +28,11 @@ ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
     conn.on('open', () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
-            chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+            chalk.green.bold('Blacky String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
         );
         
         if (!fs.existsSync('config.env')) {
-            fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
+            fs.writeFileSync('config.env', `BLACKY_SESSION="${st}"`);
         }
 
         console.log(
@@ -44,4 +44,4 @@ ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
     await conn.connect();
 }
 
-whatsAsena()
+RedBlacky()
